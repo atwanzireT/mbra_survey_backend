206eacd7d4fda90e319aab0c9044179e7cd4bc4e
atwanzireT <tatwanzire@gmail.com>
2025-05-09 12:44:10 +0300

    fixed auth

--- mbra_survey_backend/settings.py
+++ mbra_survey_backend/settings.py
@@ -131,7 +131,6 @@ MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'app.User'
 
 
 # Default primary key field type

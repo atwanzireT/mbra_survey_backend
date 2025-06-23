9f436ab5c8e1d94d7a33837ca134dc068e8f6932
atwanzireT <tatwanzire@gmail.com>
2025-06-23 09:31:35 +0300

    postgres

--- mbra_survey_backend/settings.py
+++ mbra_survey_backend/settings.py
@@ -89,10 +89,21 @@ WSGI_APPLICATION = 'mbra_survey_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mbra',  
+        'USER': 'postgres',   
+        'PASSWORD': 'atwanzire', 
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 

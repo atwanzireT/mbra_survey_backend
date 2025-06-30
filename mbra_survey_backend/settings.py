7cdcca4ede9c7601b9fcdea6bef8cbeb80154a08
atwanzireT <tatwanzire@gmail.com>
2025-06-30 22:57:13 +0300

    setting up cors

--- mbra_survey_backend/settings.py
+++ mbra_survey_backend/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     # Third Parties
     'rest_framework',
     "phonenumber_field",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -62,7 +63,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True 
 
 ROOT_URLCONF = 'mbra_survey_backend.urls'
 
